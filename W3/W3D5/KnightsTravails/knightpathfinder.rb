9ae854e6caa19a567de67c70c667af1e9e5c1af5
Jourdan Ooi <jungterooi@gmail.com>
2020-07-24 16:34:20 -0400

    finished up to phase 2 for the knightTravails

---
+++ W3/W3D5/KnightsTravails/knightpathfinder.rb
@@ -0,0 +1,61 @@
+require_relative "polytreenode"
+
+class KnightPathFinder
+    attr_reader :root_node
+
+    def initialize(pos)
+        @pos = pos
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
+        self.build_move_tree
+    end
+
+    def new_move_positions
+    end
+
+    def self.valid_moves(pos)
+        valid_moves = []
+        possible_changes = [[2,1],[1,2],[-1,-2],[-2,-1],[2,-1],[-1,2],[-2,1],[2,-1]]
+        possible_changes.each do |change|
+            x = pos[0] + change[0]
+            y = pos[1] + change[1]
+            valid_moves << [x,y] if [x,y].all? {|ele| ele >= 0 && ele < 8}
+        end
+        valid_moves
+    end
+
+    def new_move_positions(pos)
+        # debugger
+        new_moves = []
+        KnightPathFinder.valid_moves(pos).each do |ele| 
+            new_moves << ele unless @considered_positions.include?(ele)
+        end
+        @considered_positions += new_moves
+        new_moves
+    end
+
+    def build_move_tree
+        queue = [@root_node]
+        while !queue.empty?
+            current_move = queue[0]
+            p current_move.value
+            queue += self.possible_moves(current_move)
+            queue.shift
+        end
+    end
+
+    def possible_moves(node)
+        pos = node.value
+        # debugger
+        new_moves = self.new_move_positions(pos)
+        children = new_moves.map {|move| PolyTreeNode.new(move) }
+        children.each {|child| child.parent = node}
+        children
+    end
+end
+
+# knight = KnightPathFinder.new([0,0])
+# knight.build_move_tree
+
+knight2 = KnightPathFinder.new([5,6])
+p knight2.root_node
---
+++ W3/W3D5/KnightsTravails/polytreenode.rb
@@ -0,0 +1,54 @@
+require "byebug"
+
+class PolyTreeNode
+    attr_reader :parent, :value
+    attr_accessor :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        @parent.children.delete(self) if @parent != nil
+        if parent_node == nil
+            @parent = nil 
+        else
+            # @parent.children.each {|child|@parent.children.delete(child) if child == self}
+            @parent = parent_node
+            parent_node.children << self
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "This is not a child of this node" if !self.children.include?(child_node)
+        child_node.parent = nil
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        return nil if self == nil
+        self.children.each do |child|
+            # debugger
+            result = child.dfs(target_value) 
+            return result if result != nil
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = []
+        queue << self
+        while !queue.empty?
+            el = queue.shift
+            el.children.each {|child| queue << child}
+            return el if el.value == target_value
+        end
+    end
+
+end
